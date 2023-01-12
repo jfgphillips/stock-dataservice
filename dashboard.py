89bd3ee349a4275fc75882e04a152eb246823364
jfgphillips <jfgphillips@gmail.com>
2023-01-12 18:20:14 +0000

    added dockerfile

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,15 @@
+import os
 from calculation_engine import calculate_portfolio
 from portfolio_data import get_user_portfolio
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
+
+@app.route("/")
+def welcome_page():
+    page_welcome = "Welcome to MegaCorp's Portfolio Website"
+    return page_welcome
+
 
 
 @app.get("/get_portfolio_by_id")
@@ -10,4 +17,8 @@ async def global_method(portfolio_id: int):
     user_portfolio = get_user_portfolio(portfolio_id)
     result = calculate_portfolio(user_portfolio)
     return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
